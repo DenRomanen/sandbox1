21baab93eff710d28edfd899dbfab9f179421404
Vladislav Balabanovich <vladislav.svoi@gmail.com>
2019-07-22 20:12:34 +0200

    First commit

---
+++ js/data.js
@@ -0,0 +1,38 @@
+
+
+(function () {
+  const products = [
+    {
+      name: 'Товар 1',
+      text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+      cost: 100,
+    },
+    {
+      name: 'Товар 2',
+      text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+      cost: 200,
+    },
+    {
+      name: 'Товар 3',
+      text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+      cost: 300,
+    },
+    {
+      name: 'Товар 4',
+      text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+      cost: 400,
+    },
+    {
+      name: 'Товар 5',
+      text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+      cost: 500,
+    },
+    {
+      name: 'Товар 6',
+      text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+      cost: 600,
+    },
+  ];
+
+  window.data = products;
+}());
---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+(function () {
+  const cardList = document.querySelector('#card-list');
+  const template = document.querySelector('#template1').content.querySelector('div');
+
+  for (let i = 0; i < window.data.length; i++) {
+    const templateClone = template.cloneNode(true);
+    templateClone.querySelector('.card-title').textContent = window.data[i].name;
+    templateClone.querySelector('.card-text').textContent = window.data[i].text;
+    templateClone.querySelector('.btn').addEventListener('click', () => {
+      alert(`Стоимость — ${window.data[i].cost}`);
+    });
+
+    cardList.appendChild(templateClone);
+  }
+}());
